9a1adf32407329ebd558160868997d2fc4678dc9
devsalomao <christophersalomao5@gmail.com>
2025-09-11 17:50:50 -0300

    feat: area servicos na criada

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
         hostname: "cdn.cosmicjs.com",
       },
     ],
+    qualities: [100]
   },
 };
 
--- src/components/home/services/index.tsx
+++ src/components/home/services/index.tsx
@@ -6,21 +6,41 @@ import styles from "./styles.module.scss";
 
 export function Services({ object }: HomeProps) {
   return (
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h2 className={styles.title}>Sobre</h2>
-        <p>{object.metadata.about.description}</p>
-      </article>
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h2 className={styles.title}>Sobre</h2>
+          <p>{object.metadata.about.description}</p>
+        </article>
 
-      <div className={styles.bannerAbout}>
-        <Image
-          className={styles.imageAbout}
-          alt="Imagem ilustrativa sobre a RPM Motors"
-          src={object.metadata.about.banner.url}
-          quality={100}
-          fill
-        />
-      </div>
-    </section>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagem ilustrativa sobre a RPM Motors"
+            src={object.metadata.about.banner.url}
+            quality={100}
+            fill
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.titleServices}>Conheça nossos serviços</h2>
+      <section className={styles.services}>
+        {object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.service}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt={service.description}
+                src={service.image.url}
+                quality={100}
+                fill
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
